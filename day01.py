f8667f225ccb814dc5006908fa5c6c6abd635a7b
freakyfanny <fannypeterssons@icloud.com>
2023-12-01 08:54:44 +0100

    Solution for day01.py

---
+++ day01.py
@@ -0,0 +1,43 @@
+from functools import reduce
+from operator import add
+from os import environ
+
+
+def getFirstAndLastDigit(inputString):
+    firstDigit = None
+    lastDigit = None
+
+    for char in inputString:
+        if char.isdigit():
+            if firstDigit is None:
+                firstDigit = char
+            lastDigit = char
+
+    if firstDigit is None:
+        return 0
+    elif firstDigit == lastDigit:
+        return int(firstDigit * 2)
+    else:
+        return int(firstDigit + lastDigit)
+        
+def processFile(fileName):
+    results = []
+
+    # Read lines from the file
+    with open(fileName, 'r') as file:
+        lines = file.readlines()
+
+        for line in lines:
+            lineResult = getFirstAndLastDigit(line)
+            results.append(lineResult)
+
+    return results
+    
+def calculateSum(fileName):
+    results = processFile(fileName)
+    totalSum = sum(results)
+    return totalSum
+
+fileName = "puzzle-input.txt"
+total = calculateSum(fileName)
+print("Total sum of first and last digits:", total)
